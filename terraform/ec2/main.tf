3f342ad25e4db1b58cab7bb50c9528f4606c984f
krenny07 <krenessav@yahoo.com>
2022-06-02 18:06:43 +0000

    added ecr repository policy

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -144,3 +144,37 @@ resource "aws_ecr_repository" "clo835-assignment1" {
     }
   )
 }
+
+esource "aws_ecr_repository_policy" "clo835_assignment1_policy" {
+  repository = aws_ecr_repository.clo835-assignment1.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "new policy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
